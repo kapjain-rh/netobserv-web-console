784665d371e3f42f0b4e53733cfb2e8c0964d70b
Kapil Jain <kapjain@redhat.com>
2026-04-22 15:27:26 -0400

    Loki Negative test cases

--- web/cypress/views/flowcollector-status.ts
+++ web/cypress/views/flowcollector-status.ts
@@ -1,12 +1,15 @@
 export namespace flowcollectorStatusSelectors {
     export const statusIndicator = '#flowcollector-status-indicator'
+    export const statusButton = 'button[aria-label="FlowCollector status"]'
+    export const statusTooltip = '#flowcollector-status-tooltip'
     export const readyRow = '[id=Ready-row]'
     export const agentReadyRow = '[id=WaitingEBPFAgents-row]'
     export const pluginReadyRow = '[id=WaitingWebConsole-row]'
     export const monitoringReadyRow = '[id=WaitingMonitoring-row]'
     export const configIssueRow = '[id=ConfigurationIssue-row]'
-    export const configWarningAlert = '.pf-v5-c-alert.pf-m-warning.pf-m-inline'
-    export const configErrorAlert = '.pf-v5-c-alert.pf-m-danger.pf-m-inline'
+    export const flpMonolithRow = '[id=WaitingFLPMonolith-row]'
+    export const lokiStackRow = '[id=WaitingLokiStack-row]'
+    export const flpParentRow = '[id=WaitingFLPParent-row]'
 }
 
 export const flowcollectorStatusPage = {
